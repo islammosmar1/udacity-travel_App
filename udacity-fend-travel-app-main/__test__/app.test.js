ec13dc4e15121fd9c439c96f9a3304e75967fa86
islammosmar1 <124386471+islammosmar1@users.noreply.github.com>
2025-02-09 12:52:48 +0200

    Delete udacity-fend-travel-app-main directory

--- udacity-fend-travel-app-main/__test__/app.test.js
+++
@@ -1,6 +0,0 @@
-import { handleSubmit } from "../src/client/js/app"
-describe("The function should be defined", () => {
-    test("Testing the handleSubmit() function", () => {
-           expect(handleSubmit).toBeDefined();
-    })
-});
